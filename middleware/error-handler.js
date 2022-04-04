cb0514383327c719e5487c3e3a428d0eb22bde37
andy <vy.chung@hdwebsoft.co>
2022-04-04 09:14:23 +0700

    set up security packages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,33 @@
-const errorHandlerMiddleware = async (err, req, res, next) => {
-    console.log(err)
-    return res.status(500).json({ msg: 'Something went wrong, please try again' })
+const { CustomAPIError } = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const errorHandlerMiddleware = (err, req, res, next) => {
+
+  let customError = {
+    // set default
+    statusCodes: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later'
   }
-  
-  module.exports = errorHandlerMiddleware
-  
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if (err.name === 'ValidatorError') {
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+    customError.statusCodes = StatusCodes.BAD_REQUEST
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCodes = StatusCodes.BAD_REQUEST
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCodes = StatusCodes.NOT_FOUND
+  }
+
+  return res.status(customError.statusCodes).json({ msg: customError.msg })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+}
+
+module.exports = errorHandlerMiddleware
